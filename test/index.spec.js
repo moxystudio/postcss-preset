4d62f6894a8aaab5bb03b79dfea5d0685d0bd17e
Nelson Fonseca <nelson.fonseca@moxy.studio>
2018-01-09 11:57:27 +0000

    feat: add postcss-for

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,6 +6,7 @@ jest.mock('postcss-import', () => (options) => ['postcss-import', options].filte
 jest.mock('postcss-mixins', () => (options) => ['postcss-mixins', options].filter((val) => val));
 jest.mock('postcss-css-variables', () => (options) => ['postcss-css-variables', options].filter((val) => val));
 jest.mock('postcss-conditionals', () => () => 'postcss-conditionals');
+jest.mock('postcss-for', () => () => 'postcss-for');
 jest.mock('postcss-cssnext', () => (options) => ['postcss-cssnext', options].filter((val) => val));
 
 afterEach(jest.resetAllMocks);
