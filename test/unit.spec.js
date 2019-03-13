a5429b37a507071d60e540d249c300427541f5f9
André Cruz <andremiguelcruz@msn.com>
2019-03-13 10:53:46 +0000

    feat: use postcss-preset-env instead of postcss-next (#28)
    BREAKING CHANGE: remove cssVariables option because postcss-preset-env will enable it based on the browsers
    BREAKING CHANGE: remove postcss-for plugin
    BREAKING CHANGE: we now postcss-preset-env with only stage 4 features, meaning most postcss-next plugins are no longer enabled
    
    Also update dependencies.
    
    Closes #18

--- test/unit.spec.js
+++ test/unit.spec.js
@@ -5,11 +5,9 @@ const preset = require('..');
 jest.mock('postcss-import', () => (options) => ['postcss-import', options].filter((val) => val));
 jest.mock('postcss-url', () => (options) => ['postcss-url', options].filter((val) => val));
 jest.mock('postcss-mixins', () => (options) => ['postcss-mixins', options].filter((val) => val));
-jest.mock('postcss-nesting', () => (options) => ['postcss-nesting', options].filter((val) => val));
-jest.mock('postcss-css-variables', () => (options) => ['postcss-css-variables', options].filter((val) => val));
-jest.mock('postcss-for', () => (options) => ['postcss-for', options].filter((val) => val));
 jest.mock('postcss-conditionals', () => (options) => ['postcss-conditionals', options].filter((val) => val));
-jest.mock('postcss-cssnext', () => (options) => ['postcss-cssnext', options].filter((val) => val));
+jest.mock('postcss-preset-env', () => (options) => ['postcss-preset-env', options].filter((val) => val));
+jest.mock('postcss-color-function', () => (options) => ['postcss-color-function', options].filter((val) => val));
 
 afterEach(jest.resetAllMocks);
 
@@ -31,14 +29,6 @@ it('should enable postcss-url if options.url is true', () => {
 
 it('should pass options.url to postcss-url', () => {
     expect(preset({ url: { foo: 'bar' } })).toMatchSnapshot();
-});
-
-it('should disable postcss-css-variables if options.cssVariables is false', () => {
-    expect(preset({ cssVariables: false })).toMatchSnapshot();
-});
-
-it('should pass options.cssVariables to postcss-css-variables', () => {
-    expect(preset({ cssVariables: { foo: 'bar' } })).toMatchSnapshot();
 });
 
 it('should pass options.browsers to postcss-cssnext ', () => {
